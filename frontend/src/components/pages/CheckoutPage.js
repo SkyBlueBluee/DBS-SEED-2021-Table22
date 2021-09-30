50a76c77fca1bd52bd8494667eba5970cd8cf22f
Joseph Cheng <chengjosephjy@gmail.com>
2021-09-30 13:40:16 +0800

    Update checkout page ui

--- frontend/src/components/pages/CheckoutPage.js
+++ frontend/src/components/pages/CheckoutPage.js
@@ -1,15 +1,45 @@
-import React from 'react'
-import { Card, Container, ListGroup } from "react-bootstrap";
-import style from "./CheckoutPage.css"
+import React, {useEffect, useState} from 'react'
+
+import { Button, Card, Container, Row, Col, ListGroup } from "react-bootstrap";
+import style from "./CheckoutPage.module.css"
 const CheckoutPage = () => {
+    [tmpCount, setTmpCount] = useState(0);
+
+
     return (
-        <Card style={{ width: '18rem' }}>
-                <ListGroup variant="flush">
-                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                </ListGroup>
-            </Card>
+        <Container className={style.wrapper}>
+            <Row>
+                <Col>
+                    <Card style={{ width: "100%" }}>
+                        <Card.Header>
+                            <Card.Title>Item Name</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <Col>
+                                    <Card.Text>
+                                        image here maybe
+                                    </Card.Text>
+                                </Col>
+                                <Col className="justify-content-md-right">
+                                    <Row className="justify-content-md-center">
+                                        <Col md="auto"> <Button variant="secondary" onClick {}>-</Button> </Col>
+                                        <Col md="auto"><Card.Text>{tmpCount}</Card.Text></Col>
+                                        <Col md="auto"><Button variant="secondary">+</Button></Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className={style.buttonRow}>
+                <Col md="auto">
+                    <Button variant="primary">Checkout</Button>
+                </Col>
+            </Row>
+        </Container>
+
     )
 }
 
