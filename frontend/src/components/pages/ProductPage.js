b075ad3b49536fc69843b813f53efa6a5fe8befb
mavissoon001 <mavissyn@gmail.com>
2021-09-30 13:56:59 +0800

    updated cardgroup

--- frontend/src/components/pages/ProductPage.js
+++ frontend/src/components/pages/ProductPage.js
@@ -1,51 +1,42 @@
 import React from 'react'
-import {Card, CardGroup} from 'react-bootstrap';
+import { Card, CardGroup } from 'react-bootstrap';
+import "../../productPageStyles.css"
 
 const ProductPage = () => {
+    let item1 = {
+        name: "Item 1",
+        description: "This is an item and it's nubmer is 1",
+        quantity: 1
+    };
+    let item2 = {
+        name: "Item 2",
+        description: "This is an item and it's nubmer is 2",
+        quantity: 2
+    };
+    let item3 = {
+        name: "Item 3",
+        description: "This is an item and it's nubmer is 3",
+        quantity: 3
+    };
+
+    let fakeItems = [item1, item2, item3];
+    // TODO: Style this part too @JHoweWowe
+    const listItems = fakeItems.map((item) =>
+        <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+            <Card.Title>{item.name}</Card.Title>
+            <Card.Text>{item.description}</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+            <small>{item.quantity}</small>
+        </Card.Footer>
+        </Card>
+    );
     return (
-        <div>
-            <header>PRODUCT PAGE</header>
+        <div id="cardBody">
             <CardGroup>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This card has supporting text below as a natural lead-in to additional
-                    content.{' '}
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This card has even longer content than the first to
-                    show that equal height action.
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
+                {listItems}
             </CardGroup>
         </div>
     )
