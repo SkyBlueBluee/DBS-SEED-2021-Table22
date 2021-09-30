a7cab51dd85443724f63103a0c3d25b279455403
shauncheee <54643489+shauncheee@users.noreply.github.com>
2021-09-30 14:54:31 +0800

    Sever side index.js fix

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const db = require('./db')
+const cartRouter = require('./routes/cart-router')
+const categoriesRouter = require('./routes/categories-router')
+const customerRouter = require('./routes/customers-router')
+const orderRouter = require('./routes/order-router')
+const productRouter = require('./routes/product-router')
+
 
 const app = express()
 const apiPort = 3000
@@ -16,5 +22,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', cartRouter)
+app.use('/api', categoriesRouter)
+app.use('/api', customerRouter)
+app.use('/api', orderRouter)
+app.use('/api', productRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
