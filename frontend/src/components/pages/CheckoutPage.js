9f11634c33554f20bc497f305fb26f7421193086
Joseph Cheng <chengjosephjy@gmail.com>
2021-09-30 13:47:29 +0800

    Can display array of items

--- frontend/src/components/pages/CheckoutPage.js
+++ frontend/src/components/pages/CheckoutPage.js
@@ -1,36 +1,59 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Button, Card, Container, Row, Col, ListGroup } from "react-bootstrap";
 import style from "./CheckoutPage.module.css"
 const CheckoutPage = () => {
-    [tmpCount, setTmpCount] = useState(0);
 
+    
+    let item1 = {
+        name: "Item 1",
+        description: "This is an item and it's nubmer is 1",
+        quantity: 1
+    };
 
+    let item2 = {
+        name: "Item 2",
+        description: "This is an item and it's nubmer is 2",
+        quantity: 2
+    };
+    let item3 = {
+        name: "Item 3",
+        description: "This is an item and it's nubmer is 3",
+        quantity: 3
+    };
+    let fakeItems = [item1, item2, item3];
+
+    // TODO: Style this part too @JHoweWowe
+    const listItems = fakeItems.map((item) =>
+        <Card style={{ width: "100%" }}>
+            <Card.Header>
+                <Card.Title>{item.name}</Card.Title>
+            </Card.Header>
+            <Card.Body>
+                <Row>
+                    <Col>
+                        <Card.Text>
+                            {item.description}
+                        </Card.Text>
+                    </Col>
+                    <Col className="justify-content-md-right">
+                        <Row className="justify-content-md-center">
+                            <Col md="auto"> <Button variant="secondary">-</Button> </Col>
+                            <Col md="auto"><Card.Text>{item.quantity}</Card.Text></Col>
+                            <Col md="auto"><Button variant="secondary">+</Button></Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    );
+
+    let [tmpCount, setTmpCount] = useState(0);
     return (
         <Container className={style.wrapper}>
             <Row>
                 <Col>
-                    <Card style={{ width: "100%" }}>
-                        <Card.Header>
-                            <Card.Title>Item Name</Card.Title>
-                        </Card.Header>
-                        <Card.Body>
-                            <Row>
-                                <Col>
-                                    <Card.Text>
-                                        image here maybe
-                                    </Card.Text>
-                                </Col>
-                                <Col className="justify-content-md-right">
-                                    <Row className="justify-content-md-center">
-                                        <Col md="auto"> <Button variant="secondary" onClick {}>-</Button> </Col>
-                                        <Col md="auto"><Card.Text>{tmpCount}</Card.Text></Col>
-                                        <Col md="auto"><Button variant="secondary">+</Button></Col>
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Card.Body>
-                    </Card>
+                    {listItems}
                 </Col>
             </Row>
             <Row className={style.buttonRow}>
