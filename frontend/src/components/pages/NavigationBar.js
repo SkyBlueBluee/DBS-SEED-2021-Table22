28b31fc4b1a8706d3ef6ea34265572fd1b538415
Joseph Cheng <chengjosephjy@gmail.com>
2021-09-30 16:03:16 +0800

    Login done from server

--- frontend/src/components/pages/NavigationBar.js
+++ frontend/src/components/pages/NavigationBar.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useHistory,
+    useLocation
+  } from "react-router-dom";
+
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 const NavigationBar = () => {
+    let [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        
+    });
     return (
         <div>
             <Navbar expand="lg">
-                <Navbar.Brand href="#home">Page name maybe?</Navbar.Brand>
+                <Navbar.Brand href="#home">Digital Web Marketplace</Navbar.Brand>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Nav className="me-auto">
