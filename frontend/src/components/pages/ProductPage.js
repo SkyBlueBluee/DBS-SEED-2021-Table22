d23f424f7b862ffd9cee5fe5b40f2557855295e4
mavissoon001 <mavissyn@gmail.com>
2021-09-30 14:14:55 +0800

    updated add to cart button

--- frontend/src/components/pages/ProductPage.js
+++ frontend/src/components/pages/ProductPage.js
@@ -1,35 +1,41 @@
 import React from 'react'
-import { Card, CardGroup } from 'react-bootstrap';
+import { Card, CardGroup, Button} from 'react-bootstrap';
 import "../../productPageStyles.css"
 
 const ProductPage = () => {
     let item1 = {
         name: "Item 1",
         description: "This is an item and it's nubmer is 1",
-        quantity: 1
+        quantity: 1,
+        img: "https://cdn.thesolewomens.co.uk/2021/07/nike-air-force-1-07-white-gym-red-cz0270-104_w900.jpg"
     };
     let item2 = {
         name: "Item 2",
         description: "This is an item and it's nubmer is 2",
-        quantity: 2
+        quantity: 2,
+        img: "https://cdn.thesolewomens.co.uk/2021/07/nike-air-force-1-07-white-gym-red-cz0270-104_w900.jpg"
     };
     let item3 = {
         name: "Item 3",
         description: "This is an item and it's nubmer is 3",
-        quantity: 3
+        quantity: 3,
+        img: "https://cdn.thesolewomens.co.uk/2021/07/nike-air-force-1-07-white-gym-red-cz0270-104_w900.jpg"
     };
 
     let fakeItems = [item1, item2, item3];
     // TODO: Style this part too @JHoweWowe
     const listItems = fakeItems.map((item) =>
         <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Img variant="top" src={item.img} />
         <Card.Body>
             <Card.Title>{item.name}</Card.Title>
             <Card.Text>{item.description}</Card.Text>
         </Card.Body>
         <Card.Footer>
-            <small>{item.quantity}</small>
+            Qty: {item.quantity}
+            <div id="productAddButton">
+                <Button id="addButton">Add to Cart</Button>
+            </div>
         </Card.Footer>
         </Card>
     );
