77e36f07371a935a7c9d2743dfd74a463c86b9d5
mavissoon001 <mavissyn@gmail.com>
2021-09-30 14:56:15 +0800

    Update product-utils.js

--- frontend/src/components/utils/product-utils.js
+++ frontend/src/components/utils/product-utils.js
@@ -9,7 +9,7 @@ export default function getProductRows(items) {
                 {
                     items.slice(i, i + 3)
                         .map(item => (
-                            <Card>
+                            <Card class="col-3"> 
                                 <Card.Img variant="top" src={item.img} />
                                 <Card.Body>
                                     <Card.Title>{item.name}</Card.Title>
